2dbb0bcf75cb3b9c2a9fcdff132681cf48729f4d
hy-09 <harada_yuki@icloud.com>
2022-02-01 14:26:01 +0900

    restframework追加

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http.response import JsonResponse
+from rest_framework.decorators import api_view
 from .util.chart import get_content_chart
 from .util.synthesis_content_title import get_synthesized_content_title
 from .models import Book
@@ -9,25 +10,26 @@ from .models import Book
 def index(request):
     return render(request, 'index.html')
 
+@api_view(['GET'])
 def books(request):
-    if request.method == 'GET':
-        books = list(Book.objects.all().values())
-        return JsonResponse({'books': books})
+    books = list(Book.objects.all().values())
+    return JsonResponse({'books': books})
 
+
+@api_view(['POST'])
 def synthesized_content_title(request):
-    if request.method == 'POST':
-        content, title = get_synthesized_content_title(request.data['urls'], request.data['titles'])
-        data = {
-            'content': content,
-            'title': title
-        }
-        return JsonResponse(data)
+    content, title = get_synthesized_content_title(request.data['urls'], request.data['titles'])
+    data = {
+        'content': content,
+        'title': title
+    }
+    return JsonResponse(data)
 
+@api_view(['POST'])
 def content_chart(request):
-    if request.method == 'POST':
-        content, chart = get_content_chart(request.data['url'])
-        data = {
-            'content': content,
-            'chart': chart
-        }
-        return JsonResponse(data)
+    content, chart = get_content_chart(request.data['url'])
+    data = {
+        'content': content,
+        'chart': chart
+    }
+    return JsonResponse(data)
